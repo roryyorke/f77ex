e1280c182a2dfd1128f87abc8b5197a8edbdf357
Rory Yorke <roryy@rory-lenovo>
2026-02-15 10:14:23 +0200

    Move "ci" scripts into ci

--- docker-build.bash
+++
@@ -1,1 +0,0 @@
-docker run -v ~/.cache/pip:/.cache/pip -v .:/example -it --user $UID --rm --env PYTHON=python3.13 quay.io/pypa/manylinux_2_28_x86_64:latest /bin/bash -c "cd /example && ./build-and-repair.bash"
