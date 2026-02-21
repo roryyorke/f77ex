5fa686dbed4317457523f11a26510357ccdab5ee
Rory Yorke <roryy@rory-lenovo>
2026-02-21 07:16:48 +0200

    Check determinant sign

---
+++ tools/check_det.py
@@ -0,0 +1,18 @@
+# compare against scipy
+import numpy as np
+import scipy.linalg as la
+from foo import mat_det
+
+from itertools import permutations
+
+np.random.seed(123456);
+
+n = 5
+
+a = np.random.normal(size=(n, n))
+
+for p in permutations(np.arange(n)):
+    pa = a[np.array(p)]
+    ref = la.det(pa)
+    tst = mat_det(pa)
+    assert np.isclose(tst, ref), f'{ref=} {tst=}'
