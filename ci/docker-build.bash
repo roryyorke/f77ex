97cb01e8093d32c6c19c8967ecfe61cae5b694ff
Rory Yorke <roryy@rory-lenovo>
2026-02-22 11:08:36 +0200

    Rename, fix and tidy up build scripts

--- ci/docker-build.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -f ./ci/docker-build.bash ]; then
-    echo "Run this from project root"
-    exit 1;
-fi
-
-docker run -v ~/.cache/pip:/.cache/pip -v .:/example --user $UID --rm --env PYTHON=python3.13 quay.io/pypa/manylinux_2_28_x86_64:latest /bin/bash -c "cd /example && ./ci/build-and-repair.bash"
---
+++ tools/manylinux-build.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ ! -f ./tools/manylinux-build.bash ]; then
+    echo "Run this from project root"
+    exit 1;
+fi
+
+docker run -v ~/.cache/pip:/.cache/pip -v .:/example --user $UID --rm --env PYTHON=python3.13 quay.io/pypa/manylinux_2_28_x86_64:latest /bin/bash -c "cd /example && ./tools/build-and-repair.bash"
