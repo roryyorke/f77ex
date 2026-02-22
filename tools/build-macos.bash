9c5e217093e232d2cd487fba6c138c2445ff15fc
Rory Yorke <roryy@rory-lenovo>
2026-02-22 15:30:24 +0200

    Run delocate in own venv

--- tools/build-macos.bash
+++ tools/build-macos.bash
@@ -10,13 +10,22 @@ echo "--Build"
 $PYTHON -m venv $workdir/build
 source $workdir/build/bin/activate
 
-pip install --progress-bar off scikit-build-core numpy scipy_openblas32 delocate
+pip install --progress-bar off scikit-build-core numpy scipy_openblas32
 
 pip wheel --progress-bar off --verbose --wheel-dir $workdir/inwheels --no-build-isolation .
 
-OPENBLAS_DIR=$(python -c 'import scipy_openblas32; import os.path; print(scipy_openblas32.get_lib_dir())')
+deactivate
 
 echo "--Repair"
+
+# repair in a different venv
+$PYTHON -m venv $workdir/repair
+source $workdir/repair/bin/activate
+
+pip install --progress-bar off scipy_openblas32 delocate
+
+OPENBLAS_DIR=$(python -c 'import scipy_openblas32; import os.path; print(scipy_openblas32.get_lib_dir())')
+
 DYLD_LIBRARY_PATH=$OPENBLAS_DIR delocate-wheel --wheel-dir $workdir/wheelhouse $workdir/inwheels/f77ex*.whl
 
 deactivate
