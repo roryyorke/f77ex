283b046e48b5c42e7f1f5679ad5948e5b5f673cb
Rory Yorke <roryy@rory-lenovo>
2026-02-22 17:59:31 +0200

    Install built wheel in build env, run test

--- tools/build-macos.bash
+++ tools/build-macos.bash
@@ -16,34 +16,34 @@ pip wheel --progress-bar off --verbose --wheel-dir $workdir/inwheels --no-build-
 
 deactivate
 
-echo "--Repair"
+# echo "--Repair"
 
-# repair in a different venv
-$PYTHON -m venv $workdir/repair
-source $workdir/repair/bin/activate
+# # repair in a different venv
+# $PYTHON -m venv $workdir/repair
+# source $workdir/repair/bin/activate
 
-pip install --progress-bar off scipy_openblas32 delocate
+# pip install --progress-bar off scipy_openblas32 delocate
 
-OPENBLAS_DIR=$(python -c 'import scipy_openblas32; import os.path; print(scipy_openblas32.get_lib_dir())')
+# OPENBLAS_DIR=$(python -c 'import scipy_openblas32; import os.path; print(scipy_openblas32.get_lib_dir())')
 
-echo "---listdeps w/ DYLD_LIBRARY_PATH"
-DYLD_LIBRARY_PATH=$OPENBLAS_DIR delocate-listdeps --depending $workdir/inwheels/f77ex*.whl
+# echo "---listdeps w/ DYLD_LIBRARY_PATH"
+# DYLD_LIBRARY_PATH=$OPENBLAS_DIR delocate-listdeps --depending $workdir/inwheels/f77ex*.whl
 
-echo "---delocate-wheel; fails"
+# echo "---delocate-wheel; fails"
 
-DYLD_LIBRARY_PATH=$OPENBLAS_DIR delocate-wheel --wheel-dir $workdir/wheelhouse $workdir/inwheels/f77ex*.whl
+# DYLD_LIBRARY_PATH=$OPENBLAS_DIR delocate-wheel --wheel-dir $workdir/wheelhouse $workdir/inwheels/f77ex*.whl
 
-deactivate
+# deactivate
 
-echo "--Test"
+echo "--Test in build env"
 
-$PYTHON -m venv $workdir/test
-source $workdir/test/bin/activate
+# $PYTHON -m venv $workdir/test
+# source $workdir/test/bin/activate
 pip install --progress-bar off $workdir/wheelhouse/f77ex*.whl
 
 cd $workdir
 python -m f77ex.test.test_mat_det
 
-echo "--Copy"
+# echo "--Copy"
 
-cp --recursive $workdir/wheelhouse $output_dir
+# cp --recursive $workdir/wheelhouse $output_dir
