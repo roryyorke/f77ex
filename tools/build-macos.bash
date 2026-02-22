204c3e201e89ebc38f9efd68743756bd5938347c
Rory Yorke <roryy@rory-lenovo>
2026-02-22 14:58:44 +0200

    macos build script; does delocate work in build environment?

---
+++ tools/build-macos.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -euo pipefail
+
+output_dir=$PWD
+
+workdir=$(mktemp --tmpdir --directory)
+
+echo "--Build"
+$PYTHON -m venv $workdir/build
+source $workdir/build/bin/activate
+
+pip install --progress-bar off scikit-build-core numpy scipy_openblas32 delocate
+
+pip wheel --progress-bar off --verbose --wheel-dir $workdir/inwheels --no-build-isolation .
+
+OPENBLAS_DIR=$(python -c 'import scipy_openblas32; import os.path; print(scipy_openblas32.get_lib_dir())')
+
+echo "--Repair"
+LD_LIBRARY_PATH=$OPENBLAS_DIR delocate-wheel --wheel-dir $workdir/wheelhouse $workdir/inwheels/f77ex*.whl
+
+deactivate
+
+echo "--Test"
+
+$PYTHON -m venv $workdir/test
+source $workdir/test/bin/activate
+pip install --progress-bar off $workdir/wheelhouse/f77ex*.whl
+
+cd $workdir
+python -m f77ex.test.test_mat_det
+
+echo "--Copy"
+
+cp --recursive $workdir/wheelhouse $output_dir
--- tools/manylinux-build.bash
+++ tools/manylinux-build.bash
@@ -5,4 +5,4 @@ if [ ! -f ./tools/manylinux-build.bash ]; then
     exit 1;
 fi
 
-docker run -v ~/.cache/pip:/.cache/pip -v .:/example --user $UID --rm --env PYTHON=python3.13 quay.io/pypa/manylinux_2_28_x86_64:latest /bin/bash -c "cd /example && ./tools/build-and-repair.bash"
+docker run -v ~/.cache/pip:/.cache/pip -v .:/example --user $UID --rm --env PYTHON=python3.13 quay.io/pypa/manylinux_2_28_x86_64:latest /bin/bash -c "cd /example && ./tools/build-linux.bash"
