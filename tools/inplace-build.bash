5a7b8492b237cb95fb0bd0a5b272cc32600adfad
Rory Yorke <roryy@rory-lenovo>
2026-03-01 09:21:16 +0200

    Fix CMakeLists.txt RPATH.  Better inplace build script

--- tools/inplace-build.bash
+++ tools/inplace-build.bash
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Demonstrate in-place, editable install
+#
+#  This re-uses the build directory, including CMake config, and is
+#  fastest when doing iterative development.
+#
+#  The downside is that this is somewhat fragile: although the build
+#  tools (scikit-build-core, CMake, and ninja) are pretty good about
+#  detecting and handling changes to the source, they're not perfect,
+#  and one can end up with apparently impossible results that go away
+#  when doing a clean build.
+
 set -euo pipefail
 
 if [ ! -f ./tools/inplace-build.bash ]; then
@@ -11,6 +22,8 @@ echo "--Build"
 python -m venv venv-build
 source venv-build/bin/activate
 
-pip install --progress-bar off setuptools_scm scikit-build-core numpy scipy_openblas32 auditwheel patchelf pytest scipy
+pip install --progress-bar off setuptools_scm scikit-build-core numpy scipy_openblas32 auditwheel patchelf scipy
 
-pip install --editable . --verbose --config-settings skbuild.build.verbose=true --config-settings skbuild.build-dir=build
+pip install --no-build-isolation --editable . --verbose --config-settings skbuild.build.verbose=true --config-settings skbuild.build-dir=build
+
+python -m f77ex.test.test_mat_det
