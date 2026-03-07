c6f62a8f9ede0e7efce87bc17964a1a44a40a3fd
Rory Yorke <roryy@rory-lenovo>
2026-03-07 16:31:24 +0200

    Add cibuildwheel libscipy-openblas manipulation

--- tools/cibw_before_build_macos.sh
+++ tools/cibw_before_build_macos.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
+set -ex
+
+# copied from Scipy
+# See https://github.com/scipy/scipy/issues/20852
 python -m pip install scipy-openblas32
 lib_loc=$(python -c"import scipy_openblas32; print(scipy_openblas32.get_lib_dir())")
-
-echo --file-listting of lib_loc=$lib_loc
-ls $lib
-
-echo --otool -L of lib_loc/libsci-star
-otool -L $lib_loc/libsci*
 
 install_name_tool -change @loader_path/../.dylibs/libgfortran.5.dylib @rpath/libgfortran.5.dylib $lib_loc/libsci*
 install_name_tool -change @loader_path/../.dylibs/libgcc_s.1.1.dylib @rpath/libgcc_s.1.1.dylib $lib_loc/libsci*
