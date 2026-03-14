1c8a0f590460dcc95130133c7a7225d559c1029d
Rory Yorke <roryy@rory-lenovo>
2026-03-14 07:15:01 +0200

    what does `ld --verbose` produce on macos?

--- tools/cibw_repair_wheel_macos.sh
+++ tools/cibw_repair_wheel_macos.sh
@@ -18,6 +18,10 @@ install_name_tool -change @loader_path/../.dylibs/libgfortran.5.dylib @rpath/lib
 install_name_tool -change @loader_path/../.dylibs/libgcc_s.1.1.dylib @rpath/libgcc_s.1.1.dylib $lib_loc/libsci*
 install_name_tool -change @loader_path/../.dylibs/libquadmath.0.dylib @rpath/libquadmath.0.dylib $lib_loc/libsci*
 
+echo ==ld --verbose output
+ld --verbose
+echo ==ld --verbose output end
+
 if [ ! -z $DYLD_LIBRARY_PATH ]; then
     echo "DYLD_LIBRARY_PATH set to $DYLD_LIBRARY_PATH";
     exit 1;
