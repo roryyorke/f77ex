5f11bcb521f61bbb9f61e7dc1fba394f3c0d5f21
Rory Yorke <roryy@rory-lenovo>
2026-03-14 06:59:02 +0200

    Change libfortran, etc., links in libscipy_openblas

--- tools/cibw_repair_wheel_macos.sh
+++ tools/cibw_repair_wheel_macos.sh
@@ -14,10 +14,9 @@ ls $lib_loc
 
 otool -L $lib_loc/libsci*
 
-# install -delete@loader_path/../.dylibs/libgfortran.5.dylib
-# install_name_tool -delete_rpath @loader_path/../.dylibs/libgfortran.5.dylib $lib_loc/libsci*
-# install_name_tool -delete_rpath @loader_path/../.dylibs/libgcc_s.1.1.dylib $lib_loc/libsci*
-# install_name_tool -delete_rpath @loader_path/../.dylibs/libquadmath.0.dylib $lib_loc/libsci*
+install_name_tool -change @loader_path/../.dylibs/libgfortran.5.dylib @rpath/libgfortran.5.dylib $lib_loc/libsci*
+install_name_tool -change @loader_path/../.dylibs/libgcc_s.1.1.dylib @rpath/libgcc_s.1.1.dylib $lib_loc/libsci*
+install_name_tool -change @loader_path/../.dylibs/libquadmath.0.dylib @rpath/libquadmath.0.dylib $lib_loc/libsci*
 
 # from pdfo https://github.com/pdfo/pdfo/blob/main/python/build_tools/cibw_before_build_macos.sh
 
