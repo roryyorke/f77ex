31ca32f19a18e235de792d10d981e2a51b3eb9e8
Rory Yorke <roryy@rory-lenovo>
2026-03-15 06:21:42 +0200

    those pdfo-inspired ln commands weren't doing anything...

--- tools/cibw_before_build_macos.sh
+++ tools/cibw_before_build_macos.sh
@@ -3,13 +3,8 @@
 # from pdfo https://github.com/pdfo/pdfo/blob/main/python/build_tools/cibw_before_build_macos.sh
 set -ex
 
-ARCH=$(uname -m)
-if [ "$ARCH" == "arm64" ]; then
-    sudo ln -fs /opt/homebrew/bin/gfortran-12 /opt/homebrew/bin/gfortran
-else
-    sudo ln -fs /usr/local/bin/gfortran-12 /usr/local/bin/gfortran
-fi
-
 echo ==gfortran -Wl,---verbose output
-/opt/homebrew/bin/gfortran -Wl,--verbose
+gfortran-15 -Wl,--verbose
 echo ==gfortran -Wl,---verbose output end
+
+exit 1
