5da7368879b6c0713b9bc2050712ab20c167d1e9
Rory Yorke <roryy@rory-lenovo>
2026-03-15 07:57:27 +0200

    what's installed with gfortran-15?

--- tools/cibw_before_build_macos.sh
+++ tools/cibw_before_build_macos.sh
@@ -3,8 +3,16 @@
 # from pdfo https://github.com/pdfo/pdfo/blob/main/python/build_tools/cibw_before_build_macos.sh
 set -ex
 
+FC=$(command -v gfortran-15)
+FC_DIR=$(dirname $FC)
+
+echo FC is $FC
+echo FC_DIR is $FC_DIR
+
+ls -l $FC_DIR
+
 echo ==gfortran -Wl,---help output
-gfortran-15 -Wl,--help
+gfortran-15 --help
 echo ==gfortran -Wl,---help output end
 
 exit 1
