e986002619e99f51101bb76b5dc2cf6dc32bacd5
Rory Yorke <roryy@rory-lenovo>
2026-03-15 08:37:38 +0200

    Somewhat introspective finding of gfortran libs

--- tools/cibw_repair_wheel_macos.sh
+++ tools/cibw_repair_wheel_macos.sh
@@ -10,24 +10,18 @@ wheel=$2
 python -m pip install scipy-openblas32
 lib_loc=$(python -c"import scipy_openblas32; print(scipy_openblas32.get_lib_dir())")
 
-ls $lib_loc
-
-otool -L $lib_loc/libsci*
-
 install_name_tool -change @loader_path/../.dylibs/libgfortran.5.dylib @rpath/libgfortran.5.dylib $lib_loc/libsci*
 install_name_tool -change @loader_path/../.dylibs/libgcc_s.1.1.dylib @rpath/libgcc_s.1.1.dylib $lib_loc/libsci*
 install_name_tool -change @loader_path/../.dylibs/libquadmath.0.dylib @rpath/libquadmath.0.dylib $lib_loc/libsci*
 
-echo ==gfortran -Wl,---verbose output
-gfortran -Wl,--verbose
-echo ==gfortran -Wl,---verbose output end
+gfortran_libs=$(slycot-gfortran -print-search-dirs|grep "^libraries: ="|sed s/^libraries:\ =//)
 
 if [ ! -z $DYLD_LIBRARY_PATH ]; then
-    echo "DYLD_LIBRARY_PATH set to $DYLD_LIBRARY_PATH";
+    echo "DYLD_LIBRARY_PATH already set: $DYLD_LIBRARY_PATH";
     exit 1;
 fi
 
-export DYLD_LIBRARY_PATH=$lib_loc:/opt/homebrew/Cellar/gcc/15.2.0_1/lib/gcc/current
+export DYLD_LIBRARY_PATH=$lib_loc:$gfortran_libs
 
 delocate-listdeps $wheel
 delocate-wheel -w $dest_dir $wheel
