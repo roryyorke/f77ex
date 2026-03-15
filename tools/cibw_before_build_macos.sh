9c3352c13816df903c3a7770ffebe2f1fb2c8eca
Rory Yorke <roryy@rory-lenovo>
2026-03-15 08:06:13 +0200

    -t?

--- tools/cibw_before_build_macos.sh
+++ tools/cibw_before_build_macos.sh
@@ -3,18 +3,18 @@
 # from pdfo https://github.com/pdfo/pdfo/blob/main/python/build_tools/cibw_before_build_macos.sh
 set -ex
 
-echo PATH is $PATH
+# echo PATH is $PATH
 
-FC=$(command -v gfortran-15)
-FC_DIR=$(dirname $FC)
+# FC=$(command -v gfortran-15)
+# FC_DIR=$(dirname $FC)
 
-echo FC is $FC
-echo FC_DIR is $FC_DIR
+# echo FC is $FC
+# echo FC_DIR is $FC_DIR
 
-ls -l $FC_DIR
+# ls -l $FC_DIR
 
-echo ==gfortran -Wl,---help output
-gfortran-15 --help
-echo ==gfortran -Wl,---help output end
+echo ==gfortran -Wl,-t output
+gfortran-15 -Wl,-t
+echo ==gfortran -Wl,-t output end
 
 exit 1
